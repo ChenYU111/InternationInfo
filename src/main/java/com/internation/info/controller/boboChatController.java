edf7cbd678d543c30dc9a348ff9838d55ef6cedd
chenyu6 <chenyu6@SHH-IT-BTC18CL.wilmar.cn>
2018-05-17 01:23:20 +0800

    聊天室    加了layout

--- src/main/java/com/internation/info/controller/boboChatController.java
+++ src/main/java/com/internation/info/controller/boboChatController.java
@@ -1,4 +1,4 @@
-package com.internation.info.controller.web;
+package com.internation.info.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
